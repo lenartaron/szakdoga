6e42ca60cdd585f0bcbb9a89d37b720ba1e4431c
lenartaron <83168006+lenartaron@users.noreply.github.com>
2025-01-16 12:56:02 +0100

    header and footer position

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,29 @@
 
 </script>
 <template>
-  <h1>teszt</h1>
+  <header>
+    <h1>teszt</h1>
+    <a href="http://localhost:5173/">link</a>
+  </header>
+  <main>
+
+  </main>
+  <footer>
+    <p>Copyright &copy;</p>
+  </footer>
+
 </template>
 <style scoped>
-h1 {
+header> h1 {
   cursor: url("assets/cursortest.cur"), pointer;
   color: aliceblue;
+}
+
+header>a {
+  font-weight: 700;
+  font-size: xx-large;
+  text-decoration: none;
+  color: aliceblue;
+  cursor: url("assets/cursortest.cur"), pointer;
 }
 </style>
