846797c6204e6437a21f559259b40aa4769c50ed
lenartaron <83168006+lenartaron@users.noreply.github.com>
2025-02-06 13:55:04 +0100

    card position (pc and phone), animation rebuild

--- src/components/EventCard.vue
+++ src/components/EventCard.vue
@@ -1,64 +1,40 @@
 <script setup>
 
 </script>
+
 <template>
   <main>
-      <div class="container" v-for="(index, mood) in 5">
-        <div class="card">
-          <img src="../assets/testProfilIcon.png" alt="Card Image 1" />
-          <div class="content">
-            <h3>Card Title {{ index }}</h3>
-            <p>This is a description for the first card.</p>
-          </div>
-          <button @click="" id="reszletek">szia</button>
+    <div class="container">
+      <div class="card" v-for="index in 7" :key="index">
+        <img src="../assets/testProfilIcon.png" alt="Card Image 1" />
+        <div class="content">
+          <h3>Card Title {{ index }}</h3>
+          <p>This is a description for the first card.</p>
         </div>
+        <button @click="EventDetail(index)" id="reszletek"><span>Szia</span></button>
       </div>
-    </main>
+    </div>
+  </main>
 </template>
 <style scoped>
-.page-wrapper {
-  min-height: 100vh;
-}
-
 .container {
-  align-items: center;
-  justify-content: center;
-}
-
-.page-wrapper, .container {
-  flex-direction: column;
-}
-
-.page-wrapper, .container, .card, #reszletek {
   display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
 }
 
 .card {
+  color: #a88db8;
   background-color: rgba(0, 0, 0, 0.7);
   border-radius: 10px;
   padding: 20px;
-  margin: 20px 0;
-  width: 20%;
+  margin: 20px;
+  width: 100%;
   height: auto;
   box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.2);
   align-items: flex-start;
   transition: width 1s, background-color 1s, color 1s;
-}
-
-.card, #reszletek {
-  color: white;
-}
-
-.card:hover {
-  width: 83%;
-  background-color: #fff;
-  color: #000;
-}
-
-.card:hover #reszletek {
-  background-color: #5e367400;
-  color: rgb(0, 0, 0); 
-  border-color: #5E3674;
 }
 
 .card img {
@@ -84,7 +60,6 @@
 #reszletek {
   border: none;
   background-color: rgba(0, 0, 0, 0);
-  justify-self: center;
   position: relative;
   padding: 10px 20px;
   font-size: 1rem;
@@ -94,18 +69,51 @@
   border-radius: 8px;
   cursor: pointer;
   overflow: hidden;
-  transition: all 0.3s ease;
   background: linear-gradient(#451f5a 0 0) no-repeat calc(200% - var(--p, 0%))
     100% / 200% var(--p, 0.2em);
   transition: 0.3s var(--t, 0s),
-  background-position 0.3s calc(0.3s - var(--t, 0s));
+    background-position 0.3s calc(0.3s - var(--t, 0s));
+  color: #000;
+}
+#reszletek>span {
+  color: #a88db8;
 }
 
 #reszletek:hover {
   --p: 100%;
   --t: 0.3s;
-  color: #000000;
 }
 
+#reszletek:hover>span {
+  color: #000;
+}
 
+.card:hover {
+  width: 28%;
+  background-color: #fff;
+  color: #000;
+}
+
+.card:hover>#reszletek {
+  background-color: #5E367400;
+  color: var(--t, #000);
+  border-color: #5E3674;
+}
+
+.card:hover #reszletek::after {
+  color: #000000;
+  font-weight: bold;
+}
+
+@media (min-width: 769px) {
+  .card {
+    width: 25%;
+  }
+}
+
+@media (max-width: 768px) {
+  .card {
+    width: 100%;
+  }
+}
 </style>
